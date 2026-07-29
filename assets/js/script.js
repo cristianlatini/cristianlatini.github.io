e4d22c8bd1dffc4d614d17512ceb7fb36a82f76d
Cristian Latini <cristian.latini@outlook.com>
2026-07-29 15:20:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,8 +164,30 @@ const projectsData = {
       "Google Calendar",
       "PostgreSQL"
     ]
-
   }
+ "cafe-cloud": {
+
+    title: "Inventário Automatizado de Softwares",
+
+    category: "Automação",
+
+    image: "./assets/images/softwareinventory.png",
+
+    description:
+      "Desenvolvimento de uma solução de automação para gerenciamento de inventário de softwares utilizando Lansweeper, Power Automate e SharePoint Online.
+
+"A solução automatiza a importação e atualização dos relatórios do Lansweeper, garantindo que os dados sejam processados e centralizados no SharePoint sem necessidade de intervenção manual, mantendo o inventário sempre atualizado para consultas e relatórios.",
+
+    tech: [
+     "Power Automate",
+    "Lansweeper",
+        "Sharepoint",
+        "Microsoft Lists",
+        "Excel",
+        "Microsoft 365"
+    ]
+
+  },
 
 };
 
